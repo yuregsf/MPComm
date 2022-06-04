af8bb660f30ee6ef174215cd044bc90b49aecc11
Fábio M. Costa <fmc@ufg.br>
2022-06-04 16:42:53 -0300

    Create comparisonServer.py

---
+++ comparisonServer.py
@@ -0,0 +1,31 @@
+import socket
+import pickle
+from constMP import *
+
+serverSock = socket.socket(AF_INET, SOCK_STREAM)
+serverSock.bind(SERVER_ADDR, SERVER_PORT)
+serverSock.listen(5)
+
+numMsgs = 0
+msgs = [] # each msg is a list of tuples (with the original messages received by the peer processes)
+
+# Receive the lists of messages from the peer processes
+while numMsgs < N:
+	(conn, addr) = serverSock.accept()
+	msgPack = conn.recv(2048)
+	conn.close()
+	msgs[numMsgs] = pickle.loads(msgPack)
+	numMsgs = numMsgs + 1
+
+unordered = 0
+
+# Compare the lists of messages
+for n in range(0,N_MSGS-1):
+	firstMsg = msgs[0][n]
+	for m in range(1,N-1):
+		if firstMsg != msgs[m][n]:
+			unordered = unordered + 1
+			break
+	
+	print ('Found ' + str(unordered) + ' messages')
+	
