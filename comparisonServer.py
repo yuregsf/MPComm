9704ebbcad60b8fe6a661619418f8101a8e238fd
professorfabio <fmc@ufg.br>
2022-06-04 16:57:35 -0300

    Rename variables

--- comparisonServer.py
+++ comparisonServer.py
@@ -1,8 +1,8 @@
-import socket
+from socket import *
 import pickle
 from constMP import *
 
-serverSock = socket.socket(AF_INET, SOCK_STREAM)
+serverSock = socket(AF_INET, SOCK_STREAM)
 serverSock.bind(SERVER_ADDR, SERVER_PORT)
 serverSock.listen(5)
 
@@ -14,18 +14,17 @@ while numMsgs < N:
 	(conn, addr) = serverSock.accept()
 	msgPack = conn.recv(2048)
 	conn.close()
-	msgs[numMsgs] = pickle.loads(msgPack)
+	msgs.append(pickle.loads(msgPack))
 	numMsgs = numMsgs + 1
 
 unordered = 0
 
 # Compare the lists of messages
-for n in range(0,N_MSGS-1):
-	firstMsg = msgs[0][n]
-	for m in range(1,N-1):
-		if firstMsg != msgs[m][n]:
+for j in range(0,N_MSGS-1):
+	firstMsg = msgs[0][j]
+	for i in range(1,N-1):
+		if firstMsg != msgs[i][j]:
 			unordered = unordered + 1
 			break
 	
 	print ('Found ' + str(unordered) + ' messages')
-	
