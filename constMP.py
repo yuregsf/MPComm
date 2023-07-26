fa01c82efdda24b69de9e899fd7f6a93d2dba0d4
Fábio M. Costa <fmc@ufg.br>
2023-07-26 20:47:38 -0300

    Update constMP.py

--- constMP.py
+++ constMP.py
@@ -1,6 +1,6 @@
 #To Do: Replace all this with a naming service
 PEERS = ['10.128.0.2','10.128.0.4','10.128.0.5','10.128.0.6','10.132.0.2','10.132.0.3']
-PORT = 4567
+PORT = 5678
 N = 6
 N_MSGS = 10
 SERVER_ADDR = '10.128.0.3'
