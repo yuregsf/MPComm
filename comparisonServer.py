e40150e3e7a1221a8958dba9a1b3afacf08432ca
Fábio M. Costa <fmc@ufg.br>
2023-10-12 17:51:36 -0300

    Update comparisonServer.py

--- comparisonServer.py
+++ comparisonServer.py
@@ -6,26 +6,27 @@ serverSock = socket(AF_INET, SOCK_STREAM)
 serverSock.bind(('0.0.0.0', SERVER_PORT))
 serverSock.listen(6)
 
-numMsgs = 0
-msgs = [] # each msg is a list of tuples (with the original messages received by the peer processes)
+while (1):
+	numMsgs = 0
+	msgs = [] # each msg is a list of tuples (with the original messages received by the peer processes)
 
-# Receive the lists of messages from the peer processes
-while numMsgs < N:
-	(conn, addr) = serverSock.accept()
-	msgPack = conn.recv(32768)
-	conn.close()
-	msgs.append(pickle.loads(msgPack))
-	numMsgs = numMsgs + 1
+	# Receive the lists of messages from the peer processes
+	while numMsgs < N:
+		(conn, addr) = serverSock.accept()
+		msgPack = conn.recv(32768)
+		conn.close()
+		msgs.append(pickle.loads(msgPack))
+		numMsgs = numMsgs + 1
 
-unordered = 0
+	unordered = 0
 
-# Compare the lists of messages
-for j in range(0,N_MSGS-1):
-	firstMsg = msgs[0][j]
-	for i in range(1,N-1):
-		if firstMsg != msgs[i][j]:
-			unordered = unordered + 1
-			break
+	# Compare the lists of messages
+	for j in range(0,N_MSGS-1):
+		firstMsg = msgs[0][j]
+		for i in range(1,N-1):
+			if firstMsg != msgs[i][j]:
+				unordered = unordered + 1
+				break
 	
-print ('Found ' + str(unordered) + ' unordered message rounds')
-serverSock.close()
+	print ('Found ' + str(unordered) + ' unordered message rounds')
+	serverSock.close()
