a06a935c9e88ef06dbeaf47723770b01f256fe59
Fábio M. Costa <fmc@ufg.br>
2023-10-15 14:20:33 -0300

    Adding further user control in the comparison server

--- comparisonServer.py
+++ comparisonServer.py
@@ -8,49 +8,50 @@ serverSock = socket(AF_INET, SOCK_STREAM)
 serverSock.bind(('0.0.0.0', SERVER_PORT))
 serverSock.listen(6)
 
-if len(sys.argv) != 2:
-	print ('Usage: python3 comparisonServer.py <mode> <<<<where mode is either l or r (local or remote)>>>>')
-	exit(0)
+def mainLoop():
+	cont = 1
+	while cont:
+		(mode,nMsg) = promptUser()
+		peerList = PEERS_SAME_REGION if mode == 'l' else PEERS_TWO_REGIONS
+		startPeers(peerList,mode)
+		print('Now, wait for the message logs from the communicating peers...')
+		waitForLogsAndCompare(nMsg)
+		cont = int(input('Continue? (1=Yes; 0=No) '))
+	serverSock.close()
 
-mode = ''
-if sys.argv[1] == 'l':
-	mode = 'l'
-	PEERS = PEERS_SAME_REGION
-else:
-	if sys.argv[1] == 'r':
-		mode = 'r'
-		PEERS = PEERS_TWO_REGIONS
-	else:
-		print ('Usage: python3 comparisonServer.py <mode> <<<<where mode is either l or r (local or remote)>>>>')
-		exit(0)
+def promptUser():
+	mode =''
+	while (mode != 'l' and mode != 'r'):
+		mode = input('Enter mode (l=local region; r=remote region): ')
+	nMsgs = int(input('Enter the number of messages for each peer to send: '))
+	return (mode,nMsgs)
 
-# Connect to each of the peers and send the 'initiate' signal:
-peerNumber = 0
-for peer in PEERS:
-	clientSock = socket(AF_INET, SOCK_STREAM)
-	clientSock.connect((PEERS[peerNumber], PEER_TCP_PORT))
-	msg = (peerNumber,mode)
-	msgPack = pickle.dumps(msg)
-	clientSock.send(msgPack)
-	msgPack = clientSock.recv(512)
-	print(pickle.loads(msgPack))
-	clientSock.close()
-	peerNumber = peerNumber + 1
+def startPeers(PEERS,mode):
+	# Connect to each of the peers and send the 'initiate' signal:
+	peerNumber = 0
+	for peer in PEERS:
+		clientSock = socket(AF_INET, SOCK_STREAM)
+		clientSock.connect((PEERS[peerNumber], PEER_TCP_PORT))
+		msg = (peerNumber,mode)
+		msgPack = pickle.dumps(msg)
+		clientSock.send(msgPack)
+		msgPack = clientSock.recv(512)
+		print(pickle.loads(msgPack))
+		clientSock.close()
+		peerNumber = peerNumber + 1
 
-print('Now, wait for the message logs from the communicating peers...')
-
-# Loop to wait for the message logs for comparison:
-while (1):
-	numMsgs = 0
+def waitForLogsAndCompare(N_MSGS):
+	# Loop to wait for the message logs for comparison:
+	numPeers = 0
 	msgs = [] # each msg is a list of tuples (with the original messages received by the peer processes)
 
-	# Receive the lists of messages from the peer processes
-	while numMsgs < N:
+	# Receive the logs of messages from the peer processes
+	while numPeers < N:
 		(conn, addr) = serverSock.accept()
 		msgPack = conn.recv(32768)
 		conn.close()
 		msgs.append(pickle.loads(msgPack))
-		numMsgs = numMsgs + 1
+		numPeers = numPeers + 1
 
 	unordered = 0
 
@@ -64,4 +65,6 @@ while (1):
 	
 	print ('Found ' + str(unordered) + ' unordered message rounds')
 
-serverSock.close()
+
+# Initiate server:
+mainLoop()
