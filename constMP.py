5637337a59349559db2af1da0c16db76baac24a6
Fábio M. Costa <fmc@ufg.br>
2025-05-18 17:31:45 -0300

    Update constMP.py

--- constMP.py
+++ constMP.py
@@ -1,16 +1,3 @@
-#To Do: Replace all this with a naming service
-
-# With local addresses (within the same cloud region)
-#PEERS = ['172.31.78.41','172.31.65.227','172.31.74.212','172.31.75.17','172.31.72.48','172.31.75.162']
-
-# With public addresses (in the same region of the cloud)
-# The last one is not fixed and must be changed each time the lab is restarted.
-#PEERS_SAME_REGION = ['3.219.23.96','34.226.166.158','44.198.151.245','44.219.197.47','44.220.0.53','54.175.18.26']
-
-# With public addresses (in two separate regions - last two servers in Oregon)
-#PEERS_TWO_REGIONS = ['54.81.67.14','35.153.132.198','34.228.222.41','50.16.114.56','35.88.45.185','35.81.78.194']
-
-
 PEER_UDP_PORT = 6789
 PEER_TCP_PORT = 5679
 N = 6   # Number of peers
