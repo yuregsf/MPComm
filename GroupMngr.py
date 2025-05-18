31e86c48b84ba86b4bd17b49a3195509e59c5af4
Fábio M. Costa <fmc@ufg.br>
2025-05-18 13:55:07 -0300

    Update GroupMngr.py

--- GroupMngr.py
+++ GroupMngr.py
@@ -2,31 +2,27 @@ from socket import *
 import pickle
 from constMP import *
 
-class GroupMngr:
-  port = 0
-  membership = []
-  
-  def __init__(self):
-    self.port = GROUPMNGR_TCP_PORT
-    self.serverLoop()
+port = GROUPMNGR_TCP_PORT
+membership = []
+serverLoop()
 
-  def serverLoop(self):
-    serverSock = socket(AF_INET, SOCK_STREAM)
-    serverSock.bind(('0.0.0.0', self.port))
-    serverSock.listen(6)
-    while(1):
-      (conn, addr) = serverSock.accept()
-      msgPack = conn.recv(2048)
-      conn.close()
-      req = pickle.loads(msgPack)
-      if req["op"] == "register":
-        self.membership.append((req["ipaddr"],req["port"]))
-      elif req["op"] == "list":
-        list = []
-        for m in self.membership:
-          list.append(m[1])
-        return list
-      else:
-        pass
+def serverLoop():
+  serverSock = socket(AF_INET, SOCK_STREAM)
+  serverSock.bind(('0.0.0.0', port))
+  serverSock.listen(6)
+  while(1):
+    (conn, addr) = serverSock.accept()
+    msgPack = conn.recv(2048)
+    conn.close()
+    req = pickle.loads(msgPack)
+    if req["op"] == "register":
+      membership.append((req["ipaddr"],req["port"]))
+    elif req["op"] == "list":
+      list = []
+      for m in membership:
+        list.append(m[1])
+      return list
+    else:
+      pass
         
         
