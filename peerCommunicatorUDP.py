5cd7d8f2db203f49fe8c6a87309eb9eb99d4f435
matheusmvb <65053557+MatheusMVB@users.noreply.github.com>
2025-12-09 22:07:01 -0300

    Update peerCommunicatorUDP.py
    parte 4

--- peerCommunicatorUDP.py
+++ peerCommunicatorUDP.py
@@ -7,13 +7,18 @@ import pickle
 from requests import get
 
 # ----------------------------------------------------------------------
-# VARIÁVEIS GLOBAIS PARA ORDENAÇÃO TOTAL (Relógio de Lamport e Fila de Entrega)
+# VARIÁVEIS GLOBAIS PARA ORDENAÇÃO CAUSAL (Relógio Vetorial e Fila de Entrega)
 # ----------------------------------------------------------------------
-# Variável global para o Relógio Lógico Escalar (Lamport)
-logicalClock = 0
-# Fila de prioridade para armazenar mensagens recebidas para ordenação total
-# Armazena tuplas: (received_timestamp, sender_id, operation_data)
+# Variável global para o Relógio Vetorial (V[i] é a contagem para P_i)
+vectorClock = [] 
+# Fila de espera para mensagens que não podem ser entregues causalmente
+# Stores tuples: (sender_id, received_vector, operation_data)
 deliveryQueue = [] 
+# ID do processo atual
+myself = -1 
+# N é o número total de peers (de constMP)
+# O número de mensagens a enviar
+nMsgs = 0
 # ----------------------------------------------------------------------
 
 operations = {
@@ -23,17 +28,13 @@ operations = {
         3: "query"
         }
 
-# Counter to make sure we have received handshakes from all other processes
 handShakeCount = 0
-
 PEERS = []
 
-# UDP sockets to send and receive data messages:
+# Sockets de comunicação
 sendSocket = socket(AF_INET, SOCK_DGRAM)
 recvSocket = socket(AF_INET, SOCK_DGRAM)
 recvSocket.bind(('0.0.0.0', PEER_UDP_PORT))
-
-# TCP socket to receive start signal from the comparison server:
 serverSock = socket(AF_INET, SOCK_STREAM)
 serverSock.bind(('0.0.0.0', PEER_TCP_PORT))
 serverSock.listen(1)
@@ -69,20 +70,71 @@ def getListOfPeers():
     clientSock.close()
     return PEERS
 
+# Função auxiliar para verificar a condição de entrega CAUSAL
+def can_deliver_causally(sender_id, received_vector, current_vector):
+    # Condição 1: P_i está adiantado por 1
+    # O componente do remetente no vetor da mensagem deve ser exatamente um a mais 
+    # do que o que P_j viu de P_i.
+    if received_vector[sender_id] != current_vector[sender_id] + 1:
+        return False
+    
+    # Condição 2: Todas as dependências anteriores foram vistas
+    # Para todos os outros processos k != i, o que a mensagem viu de k
+    # já deve ter sido visto (ou incorporado) por P_j.
+    for k in range(N):
+        if k != sender_id:
+            if received_vector[k] > current_vector[k]:
+                return False
+                
+    return True
+
 class MsgHandler(threading.Thread):
     def __init__(self, sock):
         threading.Thread.__init__(self)
         self.sock = sock
 
+    def process_queue_and_deliver(self, current_vector, queue, log):
+        global myself
+        
+        delivered_count = 0
+        i = 0
+        # Itera sobre a fila, verificando se alguma mensagem presa pode ser liberada
+        while i < len(queue):
+            sender_id, received_vector, operation = queue[i]
+            
+            if can_deliver_causally(sender_id, received_vector, current_vector):
+                # Entrega (aplica na réplica e registra no log)
+                delivered_msg = (received_vector, sender_id, operation)
+                log.append(delivered_msg)
+                
+                print(f'*** DELIVERED FROM QUEUE ***: P{myself} delivered {delivered_msg[2]} from P{sender_id} with vector {received_vector}')
+                
+                # Regra 3 (a): Atualizar o relógio (máximo componente a componente)
+                for k in range(N):
+                    current_vector[k] = max(current_vector[k], received_vector[k])
+                
+                # Regra 3 (b): Incrementar a própria entrada
+                current_vector[myself] = current_vector[myself] + 1
+                
+                # Remove da fila e REINICIA a verificação (o novo vetor pode desbloquear outra mensagem)
+                queue.pop(i)
+                delivered_count += 1
+                i = 0 
+            else:
+                i += 1
+        return delivered_count
+
     def run(self):
         print('Handler is ready. Waiting for the handshakes...')
         
-        # 1. CORREÇÃO: Declarações global no topo do run()
         global handShakeCount
-        global logicalClock
+        global vectorClock
         global deliveryQueue
+        global myself
+        global N
+        global nMsgs
         
-        # Wait until handshakes are received from all other processes
+        # Espera o handshake de todos os peers
         while handShakeCount < N:
             msgPack = self.sock.recv(1024)
             msg = pickle.loads(msgPack)
@@ -92,57 +144,53 @@ class MsgHandler(threading.Thread):
 
         print('Secondary Thread: Received all handshakes. Entering the loop to receive messages.')
 
-        logList = [] # Lista de mensagens FINALMENTE ENTREGUES (ordenadas)
+        logList = [] # Lista de mensagens FINALMENTE ENTREGUES (ordenadas causalmente)
         stopCount=0 
         
-        # O número total de mensagens que esperamos é N * nMsgs. 
-        # Precisamos esperar todas as mensagens para garantir a ordem total correta 
-        # antes de enviar o log.
-        expected_total_msgs = N * nMsgs
-        
+        # O loop continua até que todos os peers enviem a mensagem de parada
         while stopCount < N:
-            try:
-                msgPack = self.sock.recv(1024)   
-                msg = pickle.loads(msgPack)
-            except EOFError:
-                # Caso a conexão feche antes de receber todas as mensagens, 
-                # o que pode acontecer em cenários concorrentes.
-                break 
+            msgPack = self.sock.recv(1024)   
+            msg = pickle.loads(msgPack)
             
             if msg[0] == -1:    # count the 'stop' messages
                 stopCount = stopCount + 1
             elif len(msg) == 3:
-                # O formato da mensagem recebida é: (remetente_id, timestamp, operacao)
-                sender_id, received_timestamp, operation = msg
+                # O formato da mensagem recebida é: (remetente_id, received_vector, operacao)
+                sender_id, received_vector, operation = msg
+                # Necessário converter para lista para garantir mutabilidade e comparação
+                received_vector = list(received_vector) 
+                
+                print(f'Message received (Vector: {received_vector}) from P{sender_id}. My current vector: {vectorClock}')
                 
                 # -----------------------------------------------------------------
-                # IMPLEMENTAÇÃO DO RELÓGIO DE LAMPORT E ORDENAÇÃO
+                # IMPLEMENTAÇÃO DO RELÓGIO VETORIAL E ORDENAÇÃO CAUSAL
                 # -----------------------------------------------------------------
                 
-                # 1. Atualizar o relógio local (Regra 3a)
-                logicalClock = max(logicalClock, received_timestamp)
+                if can_deliver_causally(sender_id, received_vector, vectorClock):
+                    # Entrega imediata
+                    delivered_msg = (received_vector, sender_id, operation)
+                    logList.append(delivered_msg)
+                    
+                    print(f'*** DELIVERED IMMEDIATE ***: P{myself} delivered {delivered_msg[2]} from P{sender_id} with vector {received_vector}')
+                    
+                    # Regra 3 (a) e (b) (após a entrega)
+                    for k in range(N):
+                        vectorClock[k] = max(vectorClock[k], received_vector[k])
+                    vectorClock[myself] = vectorClock[myself] + 1
+                    
+                else:
+                    # Coloca na fila de espera
+                    deliveryQueue.append((sender_id, received_vector, operation))
+                    print(f'--- WAITING ---: P{myself} put message from P{sender_id} in queue. Queue size: {len(deliveryQueue)}')
                 
-                # 2. Incrementar o relógio local (Regra 3b)
-                logicalClock = logicalClock + 1
-
-                # print(f'Message received (Clock: {received_timestamp}) from P{sender_id}. My clock updated to: {logicalClock}')
+                # Tenta entregar mensagens presas na fila
+                self.process_queue_and_deliver(vectorClock, deliveryQueue, logList)
                 
-                # 3. Adicionar à fila de entrega (deliveryQueue)
-                # Elemento: (timestamp, sender_id, operation)
-                deliveryQueue.append((received_timestamp, sender_id, operation))
-                
-                # 4. Ordenar a fila de entrega (timestamp primário, sender_id como desempate)
-                deliveryQueue.sort(key=lambda x: (x[0], x[1]))
+                print(f"P{myself} updated vector: {vectorClock}")
+                # -----------------------------------------------------------------
         
-        # -----------------------------------------------------------------
-        # FASE DE ENTREGA FINAL (Após receber todas as mensagens 'stop')
-        # -----------------------------------------------------------------
-        # Neste ponto, todas as mensagens chegaram e estão ordenadas na fila.
-        # Entregamos todas as mensagens da fila na ordem estrita para o log.
-        
-        logList = deliveryQueue
-        
-        print(f'\nProcess: Total ordered messages delivered = {len(logList)}\n')
+        # Impressão do Log Final Ordenado
+        print(f'\nProcess: Total causally ordered messages delivered = {len(logList)}\n')
 
         # Write log file
         logFile = open('logfile.log', 'w')
@@ -162,15 +210,22 @@ class MsgHandler(threading.Thread):
 
         exit(0)
 
+# Function to wait for start signal from comparison server:
 def waitToStart():
+    global myself
+    global vectorClock
+    global nMsgs
+    
     (conn, addr) = serverSock.accept()
     msgPack = conn.recv(1024)
     msg = pickle.loads(msgPack)
     
-    global nMsgs # Necessário para o loop de recebimento na MsgHandler
+    myself = msg[0]
     nMsgs = msg[1]
     
-    myself = msg[0]
+    # Inicializa o vetor clock (tamanho N)
+    vectorClock = [0] * N
+    
     conn.send(pickle.dumps('Peer process '+str(myself)+' started.'))
     conn.close()
     return (myself,nMsgs)
@@ -178,7 +233,7 @@ def waitToStart():
 # From here, code is executed when program starts:
 registerWithGroupManager()
 def main():
-        global logicalClock
+        global logicalClocks
         while 1:
             print('Waiting for signal to start...')
             (myself, nMsgs) = waitToStart()
@@ -210,28 +265,30 @@ def main():
                 pass  # wait for the handshakes
         
             # Send a sequence of data messages to all other processes 
-            global logicalClock 
+            global vectorClock 
             
             for msgNumber in range(0, nMsgs):
                 # Wait some random time between successive messages
                 time.sleep(random.randrange(10,100)/1000)
                 
                 # -----------------------------------------------------------------
-                # IMPLEMENTAÇÃO DO RELÓGIO DE LAMPORT NO ENVIO
+                # IMPLEMENTAÇÃO DO RELÓGIO VETORIAL NO ENVIO
                 # -----------------------------------------------------------------
-                # 1. Incrementar o relógio antes de enviar (Regra 2)
-                logicalClock = logicalClock + 1
+                # 1. Incrementar a própria entrada do vetor antes de enviar
+                vectorClock[myself] = vectorClock[myself] + 1
                 
-                # 2. Carimbar a mensagem com (process_id, timestamp, operation_data)
+                # 2. Carimbar a mensagem com (process_id, vector_clock, operation_data)
                 op_data = operations[msgNumber%4]
-                msg = (myself, logicalClock, op_data) # NOVO FORMATO: (ID, TS, DATA)
+                # É importante enviar o vetor como uma tupla (immutable) para evitar 
+                # referências, embora o pickle resolva isso. Usamos a lista localmente.
+                msg = (myself, tuple(vectorClock), op_data) 
                 # -----------------------------------------------------------------
                 
                 msgPack = pickle.dumps(msg)
                 
                 for addrToSend in PEERS:
                     sendSocket.sendto(msgPack, (addrToSend,PEER_UDP_PORT))
-                    print(f'P{myself} Sent message {msgNumber} (Clock: {logicalClock})')
+                    print(f'P{myself} Sent message {msgNumber} (Vector: {vectorClock})')
         
             # Tell all processes that I have no more messages to send
             for addrToSend in PEERS:
@@ -239,4 +296,4 @@ def main():
                 msgPack = pickle.dumps(msg)
                 sendSocket.sendto(msgPack, (addrToSend,PEER_UDP_PORT))
 if __name__ == "__main__":
-    main()
+        main()
